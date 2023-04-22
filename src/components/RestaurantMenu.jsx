02dc28b3ca6197c43f89a2916a66ee585d0a9109
ANWDM <pv4003337@gmail.com>
2023-04-22 09:11:35 +0530

    cart implemented

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -20,7 +20,6 @@ const RestaurantMenu = () => {
         const data = await fetch
         (`https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=${id}&submitAction=ENTER`);
         const json = await data.json();
-        console.log(json.data)
         setMenu(json?.data?.cards[0]?.card?.card?.info);
         setHelper(json?.data?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards);
     }
